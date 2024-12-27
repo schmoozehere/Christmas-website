603f4611858fc5df41508ac95ece0abaefcc1f06
Ankit Warbhe <vcpatil719@gmail.com>
2024-12-27 23:18:38 +0530

    .

--- dist/js/navigation.js
+++
@@ -1,5 +0,0 @@
-// Handle navigation for the "Explore the Magic" button
-document.querySelector('.cta-button').addEventListener('click', () => {
-    const section = document.querySelector('.activities');
-    section.scrollIntoView({ behavior: 'smooth' });
-});
